cc8316023437538f3ac2826e8eff46db0020e7da
unknown <lutfiasli52@gmail.com>
2022-03-14 09:08:55 +0700

    project selesai

---
+++ src/pbotugaseksplorasi/PBOtugaseksplorasi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Tm emplates
+ * and open the template in the editor.
+ */
+package pbotugaseksplorasi;
+
+/**
+ *
+ * @author muhammad lutfi
+ */
+public class PBOtugaseksplorasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double bil_a, bil_b, hasil;
+        bil_a = 454.55;
+        bil_b = 546.55;
+        hasil = bil_a + bil_b;
+        System.out.print("Hasil penjumlahan ke-2 bilangan : ");
+        System.out.println(hasil);
+    
+    }
+    
+}
